de11b84f137c2853b88a204620ac08e9428408c3
Jiun Kong <bukgeuk1234@gmail.com>
2025-04-07 19:18:59 +0900

    Update 4_1_calculator_ans.cs

--- assignment4/4_1_calculator_ans.cs
+++ assignment4/4_1_calculator_ans.cs
@@ -49,9 +49,25 @@ namespace calculator
     // Calculator class to perform operations
     public class Calculator
     {
-        // ---------- TODO ----------
-        
-        // --------------------
+        public double Calculate(double num1, string op, double num2) {
+            switch (op) {
+                case "+":
+                    return num1 + num2;
+                    break;
+                case "-":
+                    return num1 - num2;
+                    break;
+                case "*":
+                    return num1 * num2;
+                    break;
+                case "/":
+                    if (num2 == 0) throw new DivideByZeroException("Division by zero is not allowed");
+                    else return num1 / num2;
+                    break;
+                default:
+                    throw new InvalidOperationException("Invalid operator");
+            }
+        }
     }
 }
 
